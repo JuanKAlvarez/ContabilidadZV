dd98d1aea3119b85b2f0a4d56661165026aa8af6
JuanKAlvarez <JuanAlvarezCuartas@hotmail.com>
2015-06-08 18:51:42 -0500

    TollBar- iniio

--- resources/views/layout/body.blade.php
+++ resources/views/layout/body.blade.php
@@ -1,7 +1,19 @@
 <body>
+
 	@include('layout.nav')
 
-	@yield('content')
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10  col-md-offset-1">
+				
+				@include('layout.toolbar')
+
+				@yield('content')
+
+			</div>
+		</div>
+	</div>	
 
 	@include('layout.script')
+
 </body>
---
+++ resources/views/layout/toolbar.blade.php
@@ -0,0 +1,35 @@
+
+<div class="row toll-bar text-center">
+	@if (Auth::guest())
+	@else
+	<a href="" class="btn  btn-primary btn-sm "> 
+		<i class="fa fa-bar-chart fa-lg"> </i> internet
+	</a>
+
+	<a href="" class="btn   btn-sm "> 
+		<i class="fa fa-bar-chart fa-lg"></i> impresora
+	</a>
+
+	<a href="" class="btn   btn-sm "> 
+		<i class="fa fa-bar-chart fa-lg"></i> papeleria
+	</a>
+
+	<a href="" class="btn   btn-sm "> 
+		<i class="fa fa-bar-chart fa-lg"></i> servicios
+	</a>
+
+	<a href="" class="btn   btn-sm "> 
+		<i class="fa fa-bar-chart fa-lg"></i> gana
+	</a>
+
+	<a href="" class="btn   btn-sm "> 
+		<i class="fa fa-bar-chart fa-lg"></i> minutos ip
+	</a>
+
+	<a href="" class="btn   btn-sm "> 
+		<i class="fa fa-bar-chart fa-lg"></i> win red
+	</a>	
+	
+	@endif
+</div>
+
