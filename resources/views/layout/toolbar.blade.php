7e46996c277feba8d533e2fd58f3398131c34b7e
JuanKAlvarez <JuanAlvarezCuartas@hotmail.com>
2015-06-08 21:13:44 -0500

    Toll Bar
    Se cambia para que solo los usuarios registrados puedan ver la barra

--- resources/views/layout/toolbar.blade.php
+++ resources/views/layout/toolbar.blade.php
@@ -1,32 +1,31 @@
 
-<div class="row toll-bar text-center">
-	@if (Auth::guest())
-	@else
-	<a href="" class="btn  btn-primary btn-sm "> 
+<div class="row toll-bar text-center ">
+	@if (! Auth::guest())
+	<a href="" class="btn btn-sm btn-material-blue-800 "> 
 		<i class="fa fa-bar-chart fa-lg"> </i> internet
 	</a>
 
-	<a href="" class="btn   btn-sm "> 
+	<a href="" class="btn btn-sm "> 
 		<i class="fa fa-bar-chart fa-lg"></i> impresora
 	</a>
 
-	<a href="" class="btn   btn-sm "> 
+	<a href="" class="btn btn-sm "> 
 		<i class="fa fa-bar-chart fa-lg"></i> papeleria
 	</a>
 
-	<a href="" class="btn   btn-sm "> 
+	<a href="" class="btn btn-sm "> 
 		<i class="fa fa-bar-chart fa-lg"></i> servicios
 	</a>
 
-	<a href="" class="btn   btn-sm "> 
+	<a href="" class="btn btn-sm "> 
 		<i class="fa fa-bar-chart fa-lg"></i> gana
 	</a>
 
-	<a href="" class="btn   btn-sm "> 
+	<a href="" class="btn btn-sm "> 
 		<i class="fa fa-bar-chart fa-lg"></i> minutos ip
 	</a>
 
-	<a href="" class="btn   btn-sm "> 
+	<a href="" class="btn btn-sm "> 
 		<i class="fa fa-bar-chart fa-lg"></i> win red
 	</a>	
 	
