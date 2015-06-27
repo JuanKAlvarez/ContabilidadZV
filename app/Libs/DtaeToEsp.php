962242d5a402f3e732746284b807e1cd66257ce3
JuanKAlvarez <JuanAlvarezCuartas@hotmail.com>
2015-06-26 22:11:28 -0500

    DateToEsp

--- app/Libs/DtaeToEsp.php
+++ app/Libs/DtaeToEsp.php
@@ -15,21 +15,21 @@ class DateToEsp
 		$dia= $fecha[2]; 
 
 		$meses = [
-			'enero',
-			'febrero',
-			'marzo',
-			'abril',
-			'mayo',
-			'junio',
-			'julio', 
-			'agosto',
-			'septiembre',
-			'octubre',
-			'noviembre',
-			'diciembre'
+			'01' => 'Enero',
+			'02' => 'Febrero',
+			'03' => 'Marzo',
+			'04' => 'Abril',
+			'05' => 'Mayo',
+			'06' => 'Junio',
+			'07' => 'Julio', 
+			'08' => 'Agosto',
+			'09' => 'Septiembre',
+			'10' => 'Octubre',
+			'11' => 'Noviembre',
+			'12' => 'Diciembre'
 			];
 
-		$fecha= "$dia de $mes del $año";
-		dd($fecha);
+		return $fecha= "$dia de $meses[$mes] del $año";
+		
 	}
 }
--- resources/views/forms/internet/ventas/index.blade.php
+++ resources/views/forms/internet/ventas/index.blade.php
@@ -3,7 +3,7 @@
 	  	{!! Form::label('fecha', 'Fecha:',[	'class' => 'col-sm-4 control-label']) !!}
 	  	<div class="col-sm-8">
 	  		{!! Form::text('fecha', \Carbon\Carbon::now('America/Bogota')->format('Y-m-d'),
-	  		 [	'class' => 'form-control datepicker']) !!}
+	  		 [	'class' => 'form-control datepicker', 'required']) !!}
 	  	</div>
 	</div>
 	
@@ -11,7 +11,7 @@
 	<div class="for-grup">
 	  	{!! Form::label('valor', 'Valor Total:',[	'class' => 'col-sm-4 control-label']) !!}
 	  	<div class="col-sm-8">
-	  		{!! Form::text('valor',  '' ,[	'class' => 'form-control']) !!}
+	  		{!! Form::text('valor',  '' ,[	'class' => 'form-control', 'required', 'autofocus']) !!}
 	  	</div>
 	</div>	
 
